e89c43a0a01ec9d8f4519f490d3688faa210d60b
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2020-11-24 11:49:13 +0100

    First steps towards config validation.

---
+++ minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 09:57:42 by vicmarti          #+#    #+#             */
+/*   Updated: 2020/11/24 11:40:58 by vicmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct	s_scn
+{
+	t_resolution	res;
+	t_ambient		amb;
+}				t_scene;
+# endif
