ef1d68bf6fe06f2e72d02e30b1c06a20ac2c6b65
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2020-11-24 14:15:27 +0100

    Flesh out some scn details.

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 09:57:42 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/24 11:40:58 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/24 14:12:34 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ typedef struct	s_scn
 {
 	t_resolution	res;
 	t_ambient		amb;
+	t_cam			cam;
+	t_light			lgt;
+	t_geometry		geo;
 }				t_scene;
 # endif
