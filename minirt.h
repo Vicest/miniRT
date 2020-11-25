7b1dcf299fa4f799dd1a6eaf25ce9533a5b34184
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2020-11-25 13:00:10 +0100

    Function to find strings in a string list and definition of global identifiers for reference.

--- minirt.h
+++ minirt.h
@@ -6,47 +6,74 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 09:57:42 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/25 10:32:44 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/25 12:57:21 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
+# include "libft/libft.h"
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef unsigned int[3] t_resolution;
-typedef double[3] t_coord;
-typedef double[3] t_vector;
+# define N_IDS 9
+
+//# define IDENTIFIERS "R,A,c,l,sp,pl,sq,cy,tr"
+
+static char				g_identifiers[N_IDS + 1][3] = {
+						"R",
+						"A",
+						"l",
+						"c",
+						"sp",
+						"pl",
+						"sq",
+						"cy",
+						"tr",
+						{'\0'}
+};
+
+typedef unsigned int	t_resolution[3];
+typedef double			t_vector[3];
+typedef double			t_coord[3];
 
 //TODO: Maybe just better using an int? (or an array)
-typedef struct	s_colour
+typedef struct			s_colour
 {
 	unsigned char	r;
 	unsigned char	g;
 	unsigned char	b;
-}				t_colour;
+}						t_colour;
 
-typedef struct s_camera
+typedef struct			s_light
+{
+	t_coord		pos;
+	t_colour	col;
+	double		ratio;
+}						t_light;
+
+typedef struct			s_camera
 {
 	t_coord			pos;
-	t_vect			dir;
+	t_vector		dir;
 	unsigned char	fov;
-}				t_camera;
+}						t_camera;
 
-typedef struct	s_ambient
+typedef struct			s_ambient
 {
 	double			ratio;
-	t_colour		colour;
-}				t_ambient;
+	t_colour		col;
+}						t_ambient;
 
-typedef struct	s_scene
+typedef struct			s_scene
 {
 	t_resolution	res;
 	t_ambient		amb;
 	t_camera		cam;
 	t_light			lgt;
-	t_geometry		geo;
-}				t_scene;
-# endif
+//	t_geometry		geo;
+}						t_scene;
+
+int						get_next_line(int fd, char **line);
+#endif
--- sources/config_reader.c
+++ sources/config_reader.c
@@ -6,23 +6,44 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 09:49:08 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/24 11:40:57 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/25 12:56:10 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minirt.h"
 
+/*
+**	Looks for a string in a null-terminated list of strings.
+*/
 
-void	add_element(t_scene scn, char **element)
+int				strfind(char *elem, char **list)
 {
-	
+	int		pos;
 
+	pos = 0;
+	if (!list || !elem)
+		return (-1);
+	while (list[pos] && 0 != ft_strcmp(elem, list[pos]))
+			pos++;
+	return (list[pos] != NULL ? pos : -1);
+}
+
+static void		add_element(t_scene scn, char **element)
+{
+	strfind(element[0], (char **)g_identifiers);
+}
+
+static int		check_conf(t_scene scn)
+{
+	return (0);
+}
 
 /*
 **	Read from the given config file to set up the scene.
 **	Checks for configuration errors.
 */
 
-int		save_conf(int conf_fd, t_scene scn)
+int				save_conf(int conf_fd, t_scene scn)
 {
 	char	*line;
 	char	**element;
