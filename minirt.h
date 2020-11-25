3ef2444aad2236232a5f7452dbfa8cf87cfb87f2
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2020-11-25 10:37:05 +0100

    Resoluton just an int array.

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 09:57:42 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/24 14:43:25 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/25 10:32:44 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct	s_res
-{
-	unsigned int	x;
-	unsigned int	x;
-}				t_res;
+typedef unsigned int[3] t_resolution;
+typedef double[3] t_coord;
+typedef double[3] t_vector;
 
 //TODO: Maybe just better using an int? (or an array)
 typedef struct	s_colour
@@ -28,24 +26,22 @@ typedef struct	s_colour
 	unsigned char	r;
 	unsigned char	g;
 	unsigned char	b;
-}				t_colour
+}				t_colour;
 
-typedef double[3] t_coord;
-typedef double[3] t_vector;
-
-typedef struct t_camera
+typedef struct s_camera
 {
 	t_coord			pos;
 	t_vect			dir;
 	unsigned char	fov;
+}				t_camera;
 
 typedef struct	s_ambient
 {
 	double			ratio;
 	t_colour		colour;
-}				t_ambient
+}				t_ambient;
 
-typedef struct	s_scn
+typedef struct	s_scene
 {
 	t_resolution	res;
 	t_ambient		amb;
