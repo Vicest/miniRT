52844b358ecbec53e7dcf7d4fde9aed69ed9bce2
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2020-11-26 09:57:16 +0100

    Changes to get a working base.

--- sources/config_reader.c
+++ sources/config_reader.c
@@ -6,10 +6,11 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 09:49:08 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/25 13:47:47 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/26 09:55:43 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include "minirt.h"
 
 /*
@@ -31,8 +32,8 @@ int				strfind(char *elem, char **list)
 
 static void		add_element(t_scene scn, char **element)
 {
-	if (ft_strcmp(element[0], "R")
-			add_resolution(scn, element);
+//	if (ft_strcmp(element[0], "R"))
+//			add_resolution(scn, element);
 	strfind(element[0], (char **)g_identifiers);
 }
 
@@ -46,17 +47,20 @@ static int		check_conf(t_scene scn)
 **	Checks for configuration errors.
 */
 
-int				save_conf(int conf_fd, t_scene scn)
+int				save_conf(char *conf_file, t_scene scn)
 {
 	char	*line;
 	char	**element;
+	int		fd;
 	int		i;
 
-	while (0 < get_next_line(conf_fd, &line))
+	if (-1 == (fd = open(conf_file, O_RDONLY)))
+		return (-1); //TODO: Errors
+	while (0 < get_next_line(fd, &line))
 	{
 		if (!(element = ft_split(line, ' ')))
 			return (1); //TODO:Error handling.
-		add_element(scn, element);
+		//add_element(scn, element);
 		i = 0;
 		while (element[i])
 			free(element[i++]);
--- sources/main.c
+++ sources/main.c
@@ -6,16 +6,22 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 12:00:09 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/11/24 13:56:41 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/11/26 09:48:29 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minirt.h"
 #include "minlibx/mlx.h"
 
 int		main(int argn, char **args)
 {
+	t_scene	scn;
 	void	*mlx_ptr;
 	void	*win_ptr;
+
+	if (argn != 1)
+		return (1); //TODO: Error handling.
+	save_conf(args[1], scn);
 
 	mlx_ptr = mlx_init();//TODO: Error hanlding plz
 	win_ptr = mlx_new_window(mlx_ptr, 1000, 600, "Ah-ha!");
