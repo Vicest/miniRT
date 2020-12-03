7679e2a643884ec907ad9d37587c3e843bccfd1a
Victor Martinez estrada <vicmarti@c1r3s5.42madrid.com>
2020-12-03 14:50:28 +0100

    Libft update utils.

---
+++ libft/ft_atof.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 13:01:44 by vicmarti          #+#    #+#             */
+/*   Updated: 2020/12/03 13:35:43 by vicmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <math.h>
+
+/*
+**	A very basic atof.
+*/
+
+double	atof(char *a)
+{
+	double	f;
+	int		i;
+	int		decimals;
+	char	sign;
+
+	sign = a[0] == '-' ? -1 : 1;
+	if (sign == -1)
+		a++;
+	i = 0;
+	while (ft_isdigit(a[i]))
+	{
+		if (f * 10 < f)
+			return (INFINITY);
+	f = 10 * f + (a[i++] - '0');
+	}
+	if (a[i++] != '.')
+		return (sign * f);
+	decimals = 1;
+	while (ft_isdigit(a[i]))
+		f += (a[i++] - '0') / pow(10, decimals++);
+	return (sign * f);
+}
---
+++ update_libft.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rfv libft
+git clone https://github.com/Vicest/libft.git libft
+rm -rfv libft/.git
+rm -rfv libft/.gitignore
