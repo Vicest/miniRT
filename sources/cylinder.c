e2aebeb9d9a0fcc33b14317d029d0aef532ea743
Victor Martinez estrada <vicmarti@c1r3s1.42madrid.com>
2021-02-19 19:34:51 +0100

    Cylinder working properly in thickness and height, and shade.

--- sources/cylinder.c
+++ sources/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 12:04:04 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/02/11 16:00:52 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/02/19 19:08:46 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ long double	cylinder_collision(void *cylinder, t_vector v)
 	long double	sol1;
 	long double	sol2;
 
-
 	c = *(t_cylinder*)cylinder;
 	vect_sub(&p_m_x1, v.orig, c.pos.orig);
 	vect_sub(&p_m_x2, v.orig, vect_sum(&p_m_x2, c.pos.orig, c.pos.dir));
@@ -33,7 +32,7 @@ long double	cylinder_collision(void *cylinder, t_vector v)
 	vect_sum(&a, cross_prod(&a, v.dir, p_m_x2), cross_prod(&a, p_m_x1, v.dir));
 	coefficients[0] = dot_prod(a, a);
 	coefficients[1] = 2 * dot_prod(a, b);
-	coefficients[2] = dot_prod(b, b) - c.h * c.r;
+	coefficients[2] = dot_prod(b, b) - c.h * c.h * c.r * c.r;
 	//sol1 = NAN;
 	//sol2 = NAN;
 	quadratic_solver(coefficients, &sol1, &sol2);
@@ -41,7 +40,16 @@ long double	cylinder_collision(void *cylinder, t_vector v)
 		sol1 = NAN;
 	if (!isnan(sol2) && (sol2 < 0.0L || equals_zero(sol2)))
 		sol2 = NAN;
-	return (fminl(sol1, sol2));
+	sol1 = fminl(sol1, sol2);
+	if (isnan(sol1))
+		return (NAN);
+	p_m_x1 = point_at_dist(v, sol1);
+	vect_sub(&p_m_x1, p_m_x1, c.pos.orig);
+	if (dot_prod(p_m_x1, c.pos.dir) < 0)//|| dot_prod(p_m_x1, c.pos.dir) > c.h * c.h)
+		return (NAN);
+	if (dot_prod(p_m_x1, c.pos.dir) > c.h * c.h)
+		return (NAN);
+	return (sol1);
 }
 
 t_vector	cylinder_normal(void *cylinder, t_coord at, t_coord facing)
@@ -49,17 +57,17 @@ t_vector	cylinder_normal(void *cylinder, t_coord at, t_coord facing)
 	t_vector	normal;
 	t_vector	aux;
 	t_cylinder	c;
-	long double	d;
 
-	(void)facing;
 	c = *(t_cylinder*)cylinder;
 	normal.orig = at;
-	aux.dir = vect_sub(&aux.dir, at, c.pos.orig);
-	d = dot_prod(c.pos.dir, aux.dir) / c.h;
-	scalar_prod(&aux.dir, d, c.pos.dir);
+	vect_sub(&aux.dir, at, c.pos.orig);
+	scalar_prod(&aux.dir, dot_prod(c.pos.dir, aux.dir) / (c.h * c.h), c.pos.dir);
 	vect_sum(&aux.dir, aux.dir, c.pos.orig);
 	vect_sub(&normal.dir, at, aux.dir);
 	normalize(&normal);
+	vect_sub(&facing, facing, at);
+	if (dot_prod(normal.dir, facing) < 0)
+		scalar_prod(&normal.dir, -1.0L, normal.dir);
 	return (normal);
 }
 
