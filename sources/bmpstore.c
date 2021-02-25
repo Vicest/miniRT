7b7e89af14fcc8c3d8574121916fe4903920917e
Victor Martinez estrada <vicmarti@c1r3s5.42madrid.com>
2021-02-25 13:55:20 +0100

    Storage as bmp images, no padding.

--- sources/bmpstore.c
+++ sources/bmpstore.c
@@ -6,54 +6,40 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 14:31:25 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/02/21 19:54:04 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:43:19 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <unistd.h>
 #include "minirt.h"
-/*
-static int	little_endian(int bige)
-{
-	int		littlee;
-	int		bpos;
-	
-	bpos = sizeof(int);
-	littlee = 0;
-	while (bpos-- > 0)
-	{
-		littlee |= bige ^ 0xff;
-		littlee = littlee << 8;
-		bige = bige >> 8;
-	}
-	return (littlee);
-}*/
 
-int			save_bmp(t_img img, char *name, int h)
+int			save_bmp(t_img img, char *name, int dim[2])
 {
-	int		deref;
+	char	head_buff[54];
+	int		aux;
+	int		size;
 	int		fd;
 
 	name = ft_strjoin(name, ".bmp");
 	if ((fd = open(name, O_CREAT | O_RDWR, 0644)) < 0)
 		perror("Error\n");
 	free(name);
-	write(fd, "BM", 2);
-	deref = /*little_endian*/(img.line_len * h + 14);
-	write(fd, &deref, 4);
-	deref = 0x52456d76;
-	write(fd, &deref, 4);
-	deref = /*little_endian*/(14);
-	write(fd, &deref, 4);
-	deref = 40;
-	write(fd, &deref, 4);
-	while(img.line_len + h > 0)
-	{
-		deref = 0x00ff00ff;
-		write(fd, &deref, 4);
-		h--;
-	}
+	size = (img.bpp * dim[0] / 32) * 4 * dim[1];
+	ft_memcpy(head_buff, "BM", 2);
+	aux = size + 54;
+	ft_memcpy(head_buff + 2, &aux, 4);
+	ft_memcpy(head_buff + 6, "\f\n\017\017\x36\0\0\0", 8);
+	ft_memcpy(head_buff + 14, "\x28\0\0\0", 4);
+	ft_memcpy(head_buff + 18, &dim[0], 4);
+	ft_memcpy(head_buff + 22, &dim[1], 4);
+	ft_memcpy(head_buff + 26, "\x01\0", 2);
+	ft_memcpy(head_buff + 28, &img.bpp, 2);
+	ft_memcpy(head_buff + 30, "\0\0\0\0", 4);
+	ft_memcpy(head_buff + 34, &size, 4);
+	ft_memcpy(head_buff + 38, "\x13\x0B\0\0\x13\x0B\0\0\0\0\0\0\0\0\0\0", 16);
+	write(fd, head_buff, 54);
+	write(fd, img.addr, size);
 	close(fd);
 	return (0);
 }
