d835a7148d41ac0854e5a6baee8c75fa90ef5d56
Victor Martinez estrada <vicmarti@c1r1s4.42madrid.com>
2021-03-08 13:13:28 +0100

    Bmp image storage works for multiple bpps with padding.

--- sources/bmpstore.c
+++ sources/bmpstore.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 14:31:25 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/03/04 14:50:17 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/03/08 13:06:03 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,48 @@
 #include <unistd.h>
 #include "minirt.h"
 
-int			save_bmp(t_img img, char *name, int dim[2])
+static void	put_header(int fd, int size, int bpp, int dim[2])
 {
 	char	head_buff[54];
-	int		aux;
-	int		size;
-	int		fd;
 
-	name = ft_strjoin(name, ".bmp");
-	if ((fd = open(name, O_CREAT | O_RDWR, 0644)) < 0)
-		perror("Error\n");
-	free(name);
-	size = (img.bpp * dim[0] / 32) * 4 * dim[1];
 	ft_memcpy(head_buff, "BM", 2);
-	aux = size + 54;
-	ft_memcpy(head_buff + 2, &aux, 4);
+	size += 54;
+	ft_memcpy(head_buff + 2, &size, 4);
+	size -= 54;
 	ft_memcpy(head_buff + 6, "\f\n\017\017\x36\0\0\0", 8);
 	ft_memcpy(head_buff + 14, "\x28\0\0\0", 4);
 	ft_memcpy(head_buff + 18, &dim[0], 4);
 	ft_memcpy(head_buff + 22, &dim[1], 4);
 	ft_memcpy(head_buff + 26, "\x01\0", 2);
-	ft_memcpy(head_buff + 28, &img.bpp, 2);
+	ft_memcpy(head_buff + 28, &bpp, 2);
 	ft_memcpy(head_buff + 30, "\0\0\0\0", 4);
 	ft_memcpy(head_buff + 34, &size, 4);
 	ft_memcpy(head_buff + 38, "\x13\x0B\0\0\x13\x0B\0\0\0\0\0\0\0\0\0\0", 16);
 	write(fd, head_buff, 54);
-	write(fd, img.addr, size);
+
+}
+
+int			save_bmp(t_img img, char *name, int dim[2])
+{
+	int		padding;
+	int		row_size;
+	int		fd;
+
+	if (NULL == (name = ft_strjoin(name, ".bmp")))
+		config_err("Bad strjoin output.\n");
+	if ((fd = open(name, O_CREAT | O_RDWR, 0644)) < 0)
+		config_err("Could not create file.\n");
+	free(name);
+	row_size = (img.bpp * dim[0] / 8);
+	padding =  4 - (row_size % 4);
+	if (padding == 4)
+		padding = 0;
+	put_header(fd, (padding + row_size) * dim[1], img.bpp, dim);
+	while (dim[1]-- > 0)
+	{
+		write(fd, img.addr + dim[1] * row_size, row_size);
+		write(fd, "\0\0\0\0", padding);
+	}
 	close(fd);
 	return (0);
 }
